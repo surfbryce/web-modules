a7b3b797520ef0b7b5a24214d22844d52193e1f6
surfbryce <80861823+surfbryce@users.noreply.github.com>
2024-04-24 17:20:16 -0700

    Updated spring implementation to make it more accurate and provide better sleep reading

--- Spring.ts
+++ Spring.ts
@@ -1,130 +1,192 @@
-// Math Standards
-const Epsilon = 1e-4
-const Pi = Math.PI
-const Tau = (Pi * 2)
-const Exp = Math.exp
-const Sin = Math.sin
-const Cos = Math.cos
-const Sqrt = Math.sqrt
+// Credits to https://github.com/Fraktality/spr/blob/master/spr.lua
+
+// Localize our Math constants
+const pi = Math.PI
+const tau = (pi * 2)
+const exp = Math.exp
+const sin = Math.sin
+const cos = Math.cos
+const sqrt = Math.sqrt
 
 // Behavior Constants
-const SleepEpsilon = 0.1 // Determines how far away from our Final value can be considered "sleeping"/"met"
+const SLEEP_OFFSET_SQ_LIMIT = (1/3840)^2 // Square of the offset sleep limit
+const SLEEP_VELOCITY_SQ_LIMIT = 1e-2^2 // Square of the velocity sleep limit
+const EPS = 1e-5 // Epsilon for stability checks around pathological frequency/damping values
 
 // Class
-class Spring {
+export default class Spring {
 	// Private Properties
-	private Velocity: number
-	private DampingRatio: number
-	private Frequency: number
-
-	private Sleeping: boolean = true
-
-	// Public Properties
-	public Position: number
-	public Final: number
+	private DampingRatio: number;
+	private Frequency: number;
+	private Goal: number;
+	private Position: number;
+	private Velocity: number;
 
 	// Constructor
-	public constructor(initial: number, dampingRatio: number, frequency: number) {
-		// Make sure we were passed a valid Spring
-		if ((dampingRatio * frequency) < 0) {
-			throw new Error("Spring does not converge.")
+	constructor(startPosition: number, frequency: number, dampingRatio: number, goal: number = startPosition) {
+		// Validate that we can converge
+		if ((frequency * dampingRatio) < 0) {
+			throw new Error("Spring will not converge")
 		}
 
-		// Update our properties
-		this.DampingRatio = dampingRatio, this.Frequency = frequency
+		// Store our initial values
+		this.DampingRatio = dampingRatio
+		this.Frequency = frequency
+		this.Goal = goal
+		this.Position = startPosition
 		this.Velocity = 0
-		this.Position = initial, this.Final = initial
 	}
 
-	public Update(deltaTime: number): number {
-		const radialFrequency = (this.Frequency * Tau)
-		const final = this.Final
+	// Public Methods
+	public Step(deltaTime: number): number {
+		/*
+			Advance the spring simulation by dt seconds.
+			Take the damped harmonic oscillator ODE:
+			   f^2*(X[t] - g) + 2*d*f*X'[t] + X''[t] = 0
+			Where X[t] is position at time t, g is target position,
+			f is undamped angular frequency, and d is damping ratio.
+			Apply constant initial conditions:
+			   X[0] = p0
+			   X'[0] = v0
+			Solve the IVP to get analytic expressions for X[t] and X'[t].
+			The solution takes one of three forms for 0<=d<1, d=1, and d>1
+		*/
+
+		const dampingRatio = this.DampingRatio
+		const frequency = (this.Frequency * tau) // Hz -> Rad/s
+		const goal = this.Goal
+		const position = this.Position
 		const velocity = this.Velocity
 
-		const offset = (this.Position - final)
-		const dampingRatio = this.DampingRatio
-		const decay = Exp(-dampingRatio * radialFrequency * deltaTime)
+		if (dampingRatio === 1) { // Critically damped
+			const q = exp(-frequency * deltaTime)
+			const w = (deltaTime * q)
 
-		let newPosition, newVelocity
+			const wScaledFrequency = (w * frequency)
+			const c0 = (q + wScaledFrequency)
+			const c2 = (q - wScaledFrequency)
+			const c3 = (w * (frequency ** 2))
 
-		if (this.DampingRatio == 1) {
-			newPosition = (((offset * (1 + radialFrequency * deltaTime) + velocity * deltaTime) * decay) + final)
-			newVelocity = ((velocity * (1 - radialFrequency * deltaTime) - offset * (radialFrequency * radialFrequency * deltaTime)) * decay)
-		} else if (this.DampingRatio < 1) {
-			const c = Sqrt(1 - (dampingRatio * dampingRatio))
+			const goalDistance = (position - goal)
+			const newPosition = ((goalDistance * c0) + (velocity * w) + goal)
+			const newVelocity = ((velocity * c2) - (goalDistance * c3))
 
-			const i = Cos(radialFrequency * c * deltaTime)
-			const j = Sin(radialFrequency * c * deltaTime)
+			this.Position = newPosition, this.Velocity = newVelocity
+			return newPosition
+		} else if (dampingRatio < 1) { // Underdamped
+			const frequencyStep = (frequency * deltaTime)
 
-			let z
-			if (c > Epsilon) {
-				z = j / c
+			const q = exp(-dampingRatio * frequencyStep)
+			const c = sqrt(1 - (dampingRatio ** 2))
+
+			const cFrequencyStep = (c * frequencyStep)
+			const i = cos(cFrequencyStep)
+			const j = sin(cFrequencyStep)
+
+			/*
+				Damping ratios approaching 1 can cause division by very small numbers.
+				To mitigate that, group terms around z=j/c and find an approximation for z.
+				Start with the definition of z:
+				   z = sin(dt*f*c)/c
+				Substitute a=dt*f:
+				   z = sin(a*c)/c
+				Take the Maclaurin expansion of z with respect to c:
+				   z = a - (a^3*c^2)/6 + (a^5*c^4)/120 + O(c^6)
+				   z ≈ a - (a^3*c^2)/6 + (a^5*c^4)/120
+				Rewrite in Horner form:
+				   z ≈ a + ((a*a)*(c*c)*(c*c)/20 - c*c)*(a*a*a)/6
+			*/
+
+			let z: number
+			if (c > EPS) {
+				z = (j / c)
 			} else {
-				const a = (deltaTime * radialFrequency)
-				z = (a + ((((a * a) * (c * c) * (c * c) / 20 - c * c) * (a * a * a)) / 6))
+				const cSquared = (c ** 2)
+				z = (
+					frequencyStep
+					+ (
+						(((((frequencyStep ** 2) * cSquared * cSquared) / 20) - cSquared) * (frequencyStep ** 3))
+						/ 6
+					)
+				)
 			}
 
-			let y
-			if ((radialFrequency * c) > Epsilon) {
-				y = (j / (radialFrequency * c))
+			/*
+				Frequencies approaching 0 present a similar problem.
+				We want an approximation for y as f approaches 0, where:
+				   y = sin(dt*f*c)/(f*c)
+				Substitute b=dt*c:
+				   y = sin(b*c)/b
+				Now reapply the process from z.
+			*/
+
+			let y: number
+			const cFrequency = (frequency * c)
+			if (cFrequency > EPS) {
+				y = (j / cFrequency)
 			} else {
-				const b = (radialFrequency * c)
-				y = (deltaTime + ((((deltaTime * deltaTime) * (b * b) * (b * b) / 20 - b * b) * (deltaTime * deltaTime * deltaTime)) / 6))
+				const cFrequencySquared = (cFrequency ** 2)
+				y = (
+					deltaTime
+					+ (
+						(((((deltaTime ** 2) * cFrequencySquared * cFrequencySquared) / 20) - cFrequencySquared) * (deltaTime ** 3))
+						/ 6
+					)
+				)
 			}
 
-			newPosition = (((offset * (i + dampingRatio * z) + velocity * y) * decay) + final)
-			newVelocity = ((velocity * (i - z * dampingRatio) - offset * (z * radialFrequency)) * decay)
+			const goalDistance = (position - goal)
+			const newPosition = ((((goalDistance * (i + (z * dampingRatio))) + (velocity * y)) * q) + goal)
+			const newVelocity = (((velocity * (i - (z * dampingRatio))) - (goalDistance * (z * frequency))) * q)
+
+			this.Position = newPosition, this.Velocity = newVelocity
+			return newPosition
 		} else { // Overdamped
-			const c = Sqrt((dampingRatio * dampingRatio) - 1)
+			const c = sqrt((dampingRatio ** 2) - 1)
 
-			const r1 = (-radialFrequency * (dampingRatio - c))
-			const r2 = (-radialFrequency * (dampingRatio + c))
+			const r1 = (-frequency * (dampingRatio - c))
+			const r2 = (-frequency * (dampingRatio + c))
 
-			const co2 = ((velocity - offset * r1) / (2 * radialFrequency * c))
-			const co1 = (offset - co2)
+			const ec1 = exp(r1 * deltaTime)
+			const ec2 = exp(r2 * deltaTime)
 
-			const e1 = (co1 * Exp(r1 * deltaTime))
-			const e2 = (co2 * Exp(r2 * deltaTime))
+			const goalDistance = (position - goal)
+			const co2 = ((velocity - (goalDistance * r1))/(2 * frequency * c))
+			const co1 = (ec1 * (goalDistance - co2))
+			const coEc2 = (co2 * ec2)
+			const newPosition = (co1 + coEc2 + goal)
+			const newVelocity = (co1 * r1 + (coEc2 * r2))
 
-			newPosition = (e1 + e2 + final)
-			newVelocity = ((e1 * r1) + (e2 * r2))
+			this.Position = newPosition, this.Velocity = newVelocity
+			return newPosition
+		}
+	}
+
+	public CanSleep(): boolean {
+		return (
+			(((this.Velocity ** 2) > SLEEP_VELOCITY_SQ_LIMIT) || (((this.Goal - this.Position) ** 2) > SLEEP_OFFSET_SQ_LIMIT))
+			? false
+			: true
+		)
+	}
+
+	public SetGoal(goal: number) {
+		this.Goal = goal
+	}
+
+	public SetDampingRatio(dampingRatio: number) {
+		if ((this.Frequency * dampingRatio) < 0) {
+			throw new Error("Spring will not converge")
 		}
 
-		this.Position = newPosition
-		this.Velocity = newVelocity
-
-		this.Sleeping = (Math.abs(final - newPosition) <= SleepEpsilon)
-
-		return newPosition
+		this.DampingRatio = dampingRatio
 	}
 
-	public Set(value: number) {
-		this.Position = value, this.Final = value
-		this.Velocity = 0
-		this.Sleeping = true
-	}
-
-	public SetFrequency(value: number) {
-		// Make sure we were passed a valid Spring
-		if ((this.DampingRatio * value) < 0) {
-			throw new Error("Spring does not converge.")
+	public SetFrequency(frequency: number) {
+		if ((frequency * this.DampingRatio) < 0) {
+			throw new Error("Spring will not converge")
 		}
 
-		this.Frequency = value
-	}
-
-	public SetDampingRatio(value: number) {
-		// Make sure we were passed a valid Spring
-		if ((value * this.Frequency) < 0) {
-			throw new Error("Spring does not converge.")
-		}
-
-		this.DampingRatio = value
-	}
-
-	public IsSleeping(): boolean {
-		return this.Sleeping
+		this.Frequency = frequency
 	}
 }
-
-export default Spring
